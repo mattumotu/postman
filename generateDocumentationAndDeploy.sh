ba4cfe596964f79e26282382d630374ac406eed3
Matt Phillips <mattumotu@users.noreply.github.com>
2017-09-11 20:18:14 +0100

    temp force commit

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -78,8 +78,7 @@ doxygen $DOXYFILE 2>&1 | tee doxygen.log
 # Only upload if Doxygen successfully created the documentation.
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
-if [ -d "html" ] && [ -f "html/index.html" ]; then
-
+#if [ -d "html" ] && [ -f "html/index.html" ]; then
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
     # gh-pages branch.
@@ -95,9 +94,9 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
     # The ouput is redirected to /dev/null to hide any sensitive credential data
     # that might otherwise be exposed.
     git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
-else
-    echo '' >&2
-    echo 'Warning: No documentation (html) files have been found!' >&2
-    echo 'Warning: Not going to push the documentation to GitHub!' >&2
-    exit 1
-fi
+#else
+#    echo '' >&2
+#    echo 'Warning: No documentation (html) files have been found!' >&2
+#    echo 'Warning: Not going to push the documentation to GitHub!' >&2
+#    exit 1
+#fi
